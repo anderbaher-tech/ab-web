89032673d0fb44e74b4f56f0db700cfa29b1fe30
anderbaher-tech <tech@anderbaher.com>
2026-08-10 21:18:45 +0530

    ABTCT-492

--- admin/app/flutter_bootstrap.js
+++ admin/app/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3596392809"
+    serviceWorkerVersion: "4031951834"
   }
 });
